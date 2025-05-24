344ba84eff1d0403295df0822b7f43689f7e5c63
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-05-24 10:46:47 +0700

    Update Counter.s.sol

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -5,12 +5,15 @@ import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
+    // ini adalah file untuk deploy contract Counter
+    // kita akan deploy contract ini ke jaringan arbitrum
     Counter public counter;
 
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        vm.startBroadcast(); // untuk memulai broadcast transaksi ke jaringan arbitrum
+        // kita akan deploy contract Counter ke jaringan arbitrum
 
         counter = new Counter();
 
